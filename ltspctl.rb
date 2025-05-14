830203ec92b1a6716dae2187e9691142769142c0
Seijiro Moriyama <seijiro.moriyama@anagix.com>
2025-05-14 22:48:12 +0900

    ltsctl.rb: debug meesage (wait for file)

--- ltspctl.rb
+++ ltspctl.rb
@@ -307,7 +307,7 @@ EOF
 
   def wait_for file, start, error_message=nil
     count = 0
-    puts "file=#{file}, start=#{start}"
+    $stderr.puts "wait for file:'#{file}', start at #{start}"
     until !File.exist?(file) || (File.mtime(file) >= start) do
       $stderr.puts "mtime: #{File.mtime(file)} vs. #{start}" if File.exist? file
       # puts "count=#{count}"
